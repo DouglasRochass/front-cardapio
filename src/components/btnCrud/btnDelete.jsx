d1d339ff500cb6adaba428022cb7f2de05f32e53
DouglasRochass <124200549+DouglasRochass@users.noreply.github.com>
2023-12-04 22:50:06 -0300

    Add files via upload

--- src/components/btnCrud/btnDelete.jsx
+++ src/components/btnCrud/btnDelete.jsx
@@ -1,21 +1,44 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import "../btnAdicionar/btnAdicionar.css";
 import HomeLog from "../../pages/homeLog";
-import { useNavigate } from "react-router-dom";
 
-const BtnDeletar = () => {
+const BtnDeletar = ({ idProduto }) => {
+  const nav = useNavigate();
 
-  const nav = useNavigate()
+  const handleDeletar = async () => {
+    try {
+      const response = await axios.delete(
+        `https://funny-handkerchief-newt.cyclic.app/deletar/${idProduto}`
+      );
+
+      if (response.status === 200) {
+        // Item deletado com sucesso
+        nav("/logado");
+      } else {
+        console.error("Erro ao deletar o item:", response.data);
+        // Tratar erro ao deletar o item
+      }
+    } catch (error) {
+      console.error("Erro ao deletar o item:", error.message);
+      // Tratar erro ao deletar o item
+    }
+  };
 
   return (
     <>
-    <HomeLog/>
+      <HomeLog />
       <div className="popup">
         <div className="container-add">
           <h1>Tem certeza?</h1>
           <div className="btns">
-            <button className="BtnCancelar" 
-            onClick={() => nav("/logado")}>Cancelar</button>
-            <button className="BtnDeletar">Deletar</button>
+            <button className="BtnCancelar" onClick={() => nav("/logado")}>
+              Cancelar
+            </button>
+            <button className="BtnDeletar" onClick={handleDeletar}>
+              Deletar
+            </button>
           </div>
         </div>
       </div>
